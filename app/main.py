ceeda8d6b640a339d680db10e9b456a9f4929e20
Maarten Hubrechts <maarten.hubrechts@hotmail.com>
2024-07-05 19:14:30 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,14 +1,38 @@
+import re
 import socket
 import threading
 
 
+def process_ping(conn: socket.socket):
+    conn.send("+PONG\r\n".encode())
+
+
+def process_echo(conn: socket.socket, msg: str):
+    conn.send(f"+{msg}\r\n".encode())
+
+
+def resp_parser(conn: socket.socket, message: str):
+    commands = message.split(sep="\r\n")
+    amount_commands = int(commands[0][1:])
+    pattern = r"[\$*][0-9]"
+    commands = [re.sub(pattern, "", s).strip() for s in commands if re.sub(pattern, "", s).strip()]
+    while commands:
+        if commands[0] == "PING":
+            process_ping(conn)
+            commands = commands[1:]
+        elif commands[0] == "ECHO":
+            process_echo(conn, commands[1])
+            commands = commands[2:]
+        else:
+            commands = commands[1:]
+
+
 def handle_client(conn: socket.socket, addr):
     while True:
-        data = conn.recv(1024).decode()
-        print(f"data received: {data}")
-        if not data:
+        message = conn.recv(1024).decode()
+        if not message:
             break
-        conn.send("+PONG\r\n".encode())
+        resp_parser(conn, message)
     conn.close()
 
 
