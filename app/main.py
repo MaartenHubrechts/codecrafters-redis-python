c14718d35992b493660a8c7777bf5e7876d120b8
Maarten Hubrechts <maarten.hubrechts@hotmail.com>
2024-07-05 20:39:15 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,8 +1,61 @@
 import re
 import socket
 import threading
+import time
+from collections.abc import MutableMapping
 
-redis_store = {}
+
+class TTLDict(MutableMapping):
+    def __init__(self, *args, **kwargs):
+        self.store = dict()
+        self.ttl_store = dict()
+        self.update(dict(*args, **kwargs))
+
+    def _cleanup(self):
+        current_time = time.time() * 1000
+        expired_keys = [key for key, ttl in self.ttl_store.items() if ttl is not None and ttl < current_time]
+        for key in expired_keys:
+            del self.store[key]
+            del self.ttl_store[key]
+
+    def __getitem__(self, key):
+        self._cleanup()
+        return self.store[key]
+
+    def __setitem__(self, key, value, ttl=None):
+        self._cleanup()
+        self.store[key] = value
+        if ttl is not None:
+            self.ttl_store[key] = time.time() * 1000 + ttl
+        else:
+            self.ttl_store[key] = None
+
+    def __delitem__(self, key):
+        del self.store[key]
+        if key in self.ttl_store:
+            del self.ttl_store[key]
+
+    def __iter__(self):
+        self._cleanup()
+        return iter(self.store)
+
+    def __len__(self):
+        self._cleanup()
+        return len(self.store)
+
+    def __repr__(self):
+        self._cleanup()
+        return repr(self.store)
+
+    def set(self, key, value, ttl=None):
+        self.__setitem__(key, value, ttl)
+
+    def get(self, key, default=None):
+        self._cleanup()
+        return self.store.get(key, default)
+
+
+redis_store = TTLDict()
 
 
 def process_ping(conn: socket.socket):
@@ -13,37 +66,44 @@ def process_echo(conn: socket.socket, msg: str):
     conn.send(f"+{msg}\r\n".encode())
 
 
-def process_set(conn: socket.socket, key: str, val: str):
-    redis_store[key] = val
+def process_set(conn: socket.socket, key: str, val: str, ttl: int = None):
+    print(f"setting key {key} to value {val} with ttl {ttl}")
+    redis_store.set(key, val, ttl)
     conn.send("+OK\r\n".encode())
 
 
 def process_get(conn: socket.socket, key: str):
-    response = redis_store.get(key, False)
+    response = redis_store.get(key)
     if response:
         conn.send(f"${len(response)}\r\n{response}\r\n".encode())
     else:
-        conn.send("$-1\r\n")
+        conn.send("$-1\r\n".encode())
 
 
 def resp_parser(conn: socket.socket, message: str):
     commands = message.split(sep="\r\n")
     amount_commands = int(commands[0][1:])
-    pattern = r"[\$*][0-9]"
+    pattern = r"[\$*][0-9]*"
     commands = [re.sub(pattern, "", s).strip() for s in commands if re.sub(pattern, "", s).strip()]
+    print(f"commands are {commands}")
     while commands:
-        if commands[0] == "PING":
+        cmd = commands[0].lower()
+        if cmd == "ping":
             process_ping(conn)
             commands = commands[1:]
-        elif commands[0] == "ECHO":
+        elif cmd == "echo":
             process_echo(conn, commands[1])
             commands = commands[2:]
-        elif commands[0] == "GET":
+        elif cmd == "get":
             process_get(conn, commands[1])
             commands = commands[2:]
-        elif commands[0] == "SET":
-            process_set(conn, commands[1], commands[2])
-            commands = commands[3:]
+        elif cmd == "set":
+            if len(commands) > 3 and commands[3].lower() == "px":
+                process_set(conn, commands[1], commands[2], int(commands[4]))
+                commands = commands[5:]
+            else:
+                process_set(conn, commands[1], commands[2])
+                commands = commands[3:]
         else:
             commands = commands[1:]
 
